b36acbf2c2f7eb93cc4d86ca9df689272ebfbc44
yedil <230760@astanait.edu.kz>
2024-03-02 22:49:05 +0500

    Create Spring tools

---
+++ src/test/java/kz/aitu/endtermpro/EndtermProApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.endtermpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndtermProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
